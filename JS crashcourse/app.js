e9543c240cf290d42fa63cebea0966937da73453
MikeV <109777112+MikeV469@users.noreply.github.com>
2022-09-03 16:47:22 -0500

    first commit - eportfolio

--- JS crashcourse/app.js
+++ JS crashcourse/app.js
@@ -1,41 +1,11 @@
-const statusRef = document.querySelector(".status");
+async function firstSixIncomplete(userId) {
+  const promise = await fetch("https://jsonplaceholder.typicode.com/todos")
+  const result = await promise.json()
 
-function getSubscriptionStatus() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("VIP");
-    }, 2000);
-  });
+  const incompleteTasks = result.filter(element => !element.completed).slice(0, 6);
+
+  console.log(incompleteTasks)
+ 
 }
 
-/**
- * 1. Create a funtion called 'getVideo'
- * 2. Accept a parameter called 'subscriptionStatus'
- * 3. Return a new Promise inside of the function that:
- *    - if "VIP" resolve("show video")
- *    - if "FREE" resolve("show trailer")
- *    -otherwise reject("no video")
- * 4. console.log the result of getVideo(status) in main()
- */
-
-function getVideo(subscriptionStatus) {
-  return new Promise((resolve, reject) => {
-    if (subscriptionStatus === "VIP") {
-      resolve("show video")
-    }
-    else if (subscriptionStatus === "FREE"){
-      resolve("show trailer")
-    }
-    else {
-      reject("no video")
-    }
-  })
-}
-
-async function main() {
-  const status = await getSubscriptionStatus();
-  statusRef.innerHTML = status;
-  console.log(await getVideo(status))
-}
-
-main();
+firstSixIncomplete(6);
